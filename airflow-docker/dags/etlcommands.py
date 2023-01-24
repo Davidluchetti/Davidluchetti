ab9ee7c75bda8df9e951edea95f47f7a590c7318
David de Paula Luchetti <111445632+Davidluchetti@users.noreply.github.com>
2023-01-24 19:58:38 +0000

    test

--- airflow-docker/dags/etlcommands.py
+++ airflow-docker/dags/etlcommands.py
@@ -20,7 +20,7 @@ def is_valid(ti):
     return 'nvalid'
 
 with DAG('ETLcommands',start_date = datetime(2023,1,24),
-        schedule_interval = '30 * * * *', catchup= false) as dag:
+        schedule_interval = '30 * * * *', catchup= False) as dag:
 
     capture_account_data = PythonOperator(
         task_id = 'capture_account_data',
